f8ff1605e9d2f8fb5cd31a3a6241d0ddb47b3c96
Jeff <89746523+mftcodes@users.noreply.github.com>
2024-06-04 14:18:20 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -2,7 +2,14 @@ function setVal(){
   return 'Boom! All set!';
 }
 
+/*
 document.addEventListener('DOMContentLoaded', function() {
     var el = document.getElementById('test-textarea');
     el.value = setVal();
 }, false);
+*/
+
+window.onload = function() {
+    var el = document.getElementById('test-textarea');
+    el.value = callme();
+}
